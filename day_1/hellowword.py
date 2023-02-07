1ca07857be80a722ebe7cc5f9224f10be11816ab
Faizan <m.faizanrashid@gmail.com>
2023-02-07 12:41:04 +0500

    first commit

---
+++ day_1/hellowword.py
@@ -0,0 +1,42 @@
+import sys
+import math
+
+print(sys.version)
+a = 3
+b = 4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a%b)
+print(a/b)
+print(a**b)
+print(a//b)
+
+print('faizan')
+print('faizan family')
+print('pakistan')
+print('I am enjoying 30 days of python')
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-3j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('faizan'))
+print(type('faizan family'))
+print(type("pakistan"))
+
+print(10)                               #int
+print(9.8)                              #float
+print(4-3j)                             #complex
+print('faizan')                         #string
+print(bool('a<b'))                      #boolean
+print([1, 2])                           # List
+print(9.8, 3.14, 2.7)                   # Tuple
+print({'name':'faizan'})                # Dictionary
+print({9.8, 3.14, 2.7, 9.8 })           # Set
+
+a = [3]
+b = [4]
+print (math.dist(a, b))
+
---
+++ day_2/variables.py
@@ -0,0 +1,15 @@
+print('Day 2: 30 Days of python programming')
+
+fst_name = "faizan"
+snd_name = "rashid"
+full_name = "faizan rashid"
+country = "pakistan"
+city = "rawalpindi"
+age = 26
+year = "2023-01-07"
+is_married = True
+is_true = 2<3
+is_light_on = True
+
+
+
