e3a52366f79555ec02fb49b0278703b5dd5cf7a3
Faizan <m.faizanrashid@gmail.com>
2023-03-02 14:22:46 +0500

    adding day_15

---
+++ day_15/type_of_error.py
@@ -0,0 +1,60 @@
+############
+
+"""SyntaxError """
+
+# print 'hello world'
+
+############
+
+"""NameError"""
+
+# print(age)
+
+############
+
+"""IndexError"""
+
+# numbers = [1, 2, 3, 4, 5]
+# numbers[5]
+
+############
+"""ModuleNotFoundError"""
+# import maths
+
+
+#############
+
+"""AttributeError"""
+
+# import math
+# math.PI
+
+#############
+
+"""KeyError"""
+
+# users = {'name':'Asab', 'age':250, 'country':'Finland'}
+# users['county']
+
+############
+
+"""TypeError"""
+
+# 4 + '3'
+
+###########
+"""ImportError"""
+
+# from math import power    #pow
+
+
+##########
+"""ValueError"""
+
+# int('12a')
+
+###########
+
+"""ZeroDivisionError"""
+
+5/0
