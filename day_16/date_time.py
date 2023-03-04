f61c2d90d1833f63e0122ed3340fc86bd366dd42
Faizan <m.faizanrashid@gmail.com>
2023-03-04 11:27:29 +0500

    adding day_16

---
+++ day_16/date_time.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from datetime import date
+from datetime import time
+
+#############
+
+# today = datetime.now()
+# print(today)
+# timestamp = today.timestamp()
+# print(timestamp)
+# time_one = today.strftime("%m/%d/%Y, %H:%M:%S")
+# # mm/dd/YY H:M:S format
+# print("time one:", time_one)
+
+###########
+
+# from datetime import datetime
+# date_string = "5 December, 2019"
+# print("date_string =", date_string)
+# date_object = datetime.strptime(date_string, "%d %B, %Y")
+# print("date_object =", date_object)
+
+#############
+
+# from datetime import timedelta
+# t1 = timedelta(weeks=12, days=10, hours=4, seconds=20)
+# t2 = timedelta(days=7, hours=5, minutes=3, seconds=30)
+# t3 = t1 - t2
+# print(t1)
+# print(t2)
+# print("t3 =", t3)
+
+############
+
+# from datetime import timedelta
+# t1 = datetime.now()
+# t2 = datetime(1970, 1, 1)
+# t3 = t1 - t2
+# print(t1)
+# print(t2)
+# print("t3 =", t3)
+
+#########
+
